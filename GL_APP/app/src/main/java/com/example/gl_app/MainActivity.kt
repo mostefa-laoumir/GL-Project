1a5b8811aa396b6b7ac1206255d9ad1f59776c0b
Laoumir Mustapha Abdelkader <21nounou21@gmail.com>
2020-01-10 19:18:11 +0100

    radical changes of mainactivity

--- GL_APP/app/src/main/java/com/example/gl_app/MainActivity.kt
+++ GL_APP/app/src/main/java/com/example/gl_app/MainActivity.kt
@@ -20,7 +20,68 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+    inner class AnimalAdapter: BaseAdapter{
+        var animalList = ArrayList<Animal>()
+        var context:Context?= null
+        constructor(context: Context, animalList :ArrayList<Animal>):super(){
+            this.animalList=animalList
+            this.context = context
+        }
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val animal = animalList[position]
+            var inflater = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            if(animal.isKiller==true){
+                var myView = inflater.inflate(R.layout.animal_killer_ticket,null)
+                myView.tvNamee.text=animal.name!!
+                myView.ivAnimal.setImageResource(animal.image!!)
+                myView.tvDescription.text = animal.des!!
+                myView.ivAnimal.setOnClickListener{
+                    val intent = Intent(context, Main2Activity::class.java)
+                    intent.putExtra("name",animal.name!!)
+                    intent.putExtra("des",animal.des!!)
+                    intent.putExtra("image",animal.image!!)
+                    context!!.startActivity(intent)
+                }
+                myView.ivDel.setOnClickListener{
+                    delete(position)
+                }
 
+                return myView
+
+            }else{
+                var myView = inflater.inflate(R.layout.animal_ticket,null)
+                myView.tvNamee.text=animal.name!!
+                myView.ivAnimal.setImageResource(animal.image!!)
+                myView.tvDescription.text = animal.des!!
+                myView.ivAnimal.setOnClickListener{
+                    val intent = Intent(context, Main2Activity::class.java)
+                    intent.putExtra("name",animal.name!!)
+                    intent.putExtra("des",animal.des!!)
+                    intent.putExtra("image",animal.image!!)
+                    context!!.startActivity(intent)
+
+                }
+                myView.ivDel.setOnClickListener {
+                    delete(position)
+                }
+                return myView
+            }
+
+        }
+
+        override fun getItem(position: Int): Any {
+            return animalList[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getCount(): Int {
+            return animalList.size
+        }
+
+    }
 
 
 }
