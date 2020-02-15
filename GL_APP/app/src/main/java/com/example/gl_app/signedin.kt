9e676a233732171b90bbdd1f23fd08d8d4fa4094
mostefa <21nounou21@gmail.com>
2020-02-15 09:29:30 +0100

    fixed material comps issue

--- GL_APP/app/src/main/java/com/example/gl_app/signedin.kt
+++ GL_APP/app/src/main/java/com/example/gl_app/signedin.kt
@@ -14,20 +14,11 @@ class signedin : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signedin)
         var bundle: Bundle? =intent.extras
-            /*try{
-            name = bundle!!.getString("name")
-            tvName.text = name
-        }catch (e:Exception){
 
-        }
-        logout.setOnClickListener {
-            val intent = Intent(this,MainActivity::class.java)
-            startActivity(intent)
-            Toast.makeText(this,"Logged Out!",Toast.LENGTH_LONG).show()
-        }*/
-            signBtn.setOnClickListener {
-                
-            }
+
+            /*signBtn.setOnClickListener {
+
+            }*/
     }
     fun convertToBase64(attachment: File): String {
         return Base64.encodeToString(attachment.readBytes(), Base64.NO_WRAP)
