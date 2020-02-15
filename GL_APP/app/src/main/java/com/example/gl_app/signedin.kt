b7e253bad6624d91a4e7281043456e69f54521af
mostefa <21nounou21@gmail.com>
2020-02-15 14:45:38 +0100

    file import done

--- GL_APP/app/src/main/java/com/example/gl_app/signedin.kt
+++ GL_APP/app/src/main/java/com/example/gl_app/signedin.kt
@@ -1,12 +1,16 @@
 package com.example.gl_app
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.provider.OpenableColumns
 import android.util.Base64
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_signedin.*
+
 import java.io.File
+
 
 class signedin : AppCompatActivity() {
         var name:String? = ""
@@ -19,8 +23,46 @@ class signedin : AppCompatActivity() {
             /*signBtn.setOnClickListener {
 
             }*/
+        importBtn.setOnClickListener {
+
+            val intent = Intent()
+                .setType("*/*")
+                .setAction(Intent.ACTION_GET_CONTENT)
+
+            startActivityForResult(Intent.createChooser(intent, "Select a file"), 111)
+
+        }
     }
     fun convertToBase64(attachment: File): String {
         return Base64.encodeToString(attachment.readBytes(), Base64.NO_WRAP)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 111 && resultCode == RESULT_OK) {
+            val selectedFile = data?.data //The uri with the location of the file
+            fileNameID.text=getFileName(selectedFile!!)
+        }
+    }
+    fun getFileName(uri: Uri): String? {
+        var result: String? = null
+        if (uri.getScheme().equals("content")) {
+            val cursor: Cursor? = contentResolver.query(uri, null, null, null, null)
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                }
+            } finally {
+                cursor!!.close()
+            }
+        }
+        if (result == null) {
+            result = uri.getPath()
+            val cut = result!!.lastIndexOf('/')
+            if (cut != -1) {
+                result = result.substring(cut + 1)
+            }
+        }
+        return result
     }
 }
